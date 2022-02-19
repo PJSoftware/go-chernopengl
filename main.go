6b99f15d0293470e15cea0fac7068a0e4e5806c6
Peter Jones <pjsoftware@petesplace.id.au>
2022-02-19 13:27:22 +1000

    glCall() is not possible in Go

--- main.go
+++ main.go
@@ -28,9 +28,24 @@ func glClearError() {
 	for gl.GetError() != gl.NO_ERROR {}
 }
 
-func glCheckError() {
-	for glError := gl.GetError(); glError != gl.NO_ERROR; {
+// func glCall(x func()) {
+// 	glClearError()
+// 	x()
+// 	glPanicOnError()
+// }
+
+func glPanicOnError() {
+	errorOccurred := false
+
+	for {
+		glError := gl.GetError()
+		if glError == gl.NO_ERROR { break }
 		log.Println(fmt.Sprintf("OpenGL Error #%d", glError))
+		errorOccurred = true
+	}
+
+	if errorOccurred {
+		panic("OpenGL Error(s) detected")
 	}
 }
 
@@ -209,8 +224,9 @@ func main() {
 		gl.Clear(gl.COLOR_BUFFER_BIT)
 
 		glClearError()
-		gl.DrawElements(gl.TRIANGLES, 6, gl.INT, nil) // introduce error
-		glCheckError()
+		// glCall(gl.DrawElements(gl.TRIANGLES, 6, gl.INT, nil)) // introduce error
+		gl.DrawElements(gl.TRIANGLES, 6, gl.UNSIGNED_INT, nil) // introduce error
+		glPanicOnError()
 
 		window.SwapBuffers()
 		glfw.PollEvents()
